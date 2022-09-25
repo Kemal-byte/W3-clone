6465700e7a8c04ae1f5d4111990938cfd13f2fdf
Kemal <i.kemal.karabulut@gmail.com>
2022-09-25 16:53:06 +0200

    addeventlistener needed

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const arrow = document.getElementById("next");
+const images = document.getElementsByClassName("slides");
+// images[0].style.display = "block";
+console.log(images);
+
+window.onload = () => {
+  images[0].style.display = "block";
+};
+
+nextSlide = (x) => {
+  addEventListener("click", () => {
+    //TODO: add an event listener for changing slides.
+  });
+};
